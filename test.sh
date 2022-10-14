7e84cddea398e66b79bfd62898091b7e55488936
ipcjs.debian <gipcjs@gmail.com>
2022-10-14 11:35:57 +0800

    out md

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-out_file=out.txt
+shopt -s nullglob
+
+out_file=out.md
 echo >$out_file
 
 main() {
@@ -15,22 +17,39 @@ test() {
     # tesseract sim3.png out -l eng get.images
     # tesseract sim2.png out -l eng --dpi 200 get.images
 
-    images=(sim1.png sim2.png sim3.png sim4.jpg)
+  
+    images=(
+        sim1.png sim2.png sim3.png sim4.jpg 
+        temp/extra/*.{jpg,png}
+    )
     for image in "${images[@]}"; do
-        echo ""
+        echo "" | tee -a $out_file
         echo "## image: ${image}" | tee -a $out_file
         # psm_array=(0 1 2 3 4 5 6 7 8 9 10 11 12 13)
+
+        # for multi line
         # psm_array=(4 11)
-        psm_array=(11)
+        # psm_array=(11)
+
+        # for single line
+        # psm_array=(4 6)
+        # psm_array=(6)
+
+        psm_array=(4 6 11)
         for psm in "${psm_array[@]}"; do
-            echo ""
+            echo "" | tee -a $out_file
             echo "### psm: ${psm}" | tee -a $out_file
+            echo '```' | tee -a $out_file
+
             configs=(
                 -c load_system_dawg=false
                 -c load_freq_dawg=false
                 -c tessedit_char_whitelist=0123456789
             )
             tesseract "$image" - --psm "$psm" -l eng "${configs[@]}" get.images | tee -a $out_file
+            
+            echo '' | tee -a $out_file
+            echo '```' | tee -a $out_file
         done
     done
 
